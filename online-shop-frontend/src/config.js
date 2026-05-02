663302dece613fd54818c56347f2aee209a4ffb7
Macbook Air 2020 M1 <macbookair2020m1@MacBook-Air-Macbook.local>
2026-05-03 03:00:25 +0700

    Fix: Add missing Google Client ID

--- online-shop-frontend/src/config.js
+++ online-shop-frontend/src/config.js
@@ -1,7 +1,8 @@
 const config = {
   API_URL: window.location.hostname === "localhost" 
     ? "http://localhost:5001" 
-    : "https://online-shop-beige-one.vercel.app"
+    : "https://online-shop-beige-one.vercel.app",
+  GOOGLE_CLIENT_ID: "67311538354-3kkrjm976iaptm7k40qgr5rrgefgu2i7.apps.googleusercontent.com"
 };
 
 export default config;
